4bed28f82d318e7bd23c157cf5d9efe308b45c55
zdenek-nemec <zdenek.nemec@artin.cz>
2020-07-27 17:23:46 +0200

    #1: Experimenting with sprite renderers and UI text

--- ReactionTimeBenchmark/Assets/Scripts/GameHandler.cs
+++ ReactionTimeBenchmark/Assets/Scripts/GameHandler.cs
@@ -14,6 +14,14 @@ public class GameHandler : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Debug.Log("GameHandler.Update(): Started");
 
+        SpriteRenderer sprite;
+        sprite = GetComponentInChildren<SpriteRenderer>();
+        if (Input.GetKeyDown(KeyCode.C)) {
+            Debug.Log("GameHandler.Update(): C pressed");
+        sprite.color = new Color (1, 0, 0, 1);
+        }
+        // Debug.Log("GameHandler.Update(): Finished");
     }
 }
