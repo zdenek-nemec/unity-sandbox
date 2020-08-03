53e907e20ebba74ddecf8417d45c4d1b1a42daab
zdenek-nemec <zdenek.nemec@artin.cz>
2020-08-03 17:08:18 +0200

    #3: Health bars with border, canvas for text, 2D rigid body

---
+++ CodeMonkeyTutorial/Assets/StartShip.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartShip : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Rigidbody2D>().velocity = new Vector2(0, 10);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
