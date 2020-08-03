8df129cc2cd6cc886dba0bcf9c31f9f353dc6003
zdenek-nemec <zdenek.nemec@artin.cz>
2020-08-03 08:48:43 +0200

    #3: Simple 2D scene setup

---
+++ CodeMonkeyTutorial/Assets/Scripts/Tutorial07GameHandler.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CodeMonkey;
+using CodeMonkey.Utils;
+
+public class Tutorial07GameHandler : MonoBehaviour {
+    void Start() {
+        Debug.Log("GameHandler.Start");
+
+        int i = 0;
+        FunctionPeriodic.Create(() => {
+            CMDebug.TextPopupMouse("Ding " + i + "!");
+            i++;
+        }, .5f);
+    }
+
+    void Update() {
+
+    }
+}
