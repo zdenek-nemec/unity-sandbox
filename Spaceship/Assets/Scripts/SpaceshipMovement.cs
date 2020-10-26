3f087257237b3056be93a97a84908aaaf2af0ecc
zdenek-nemec <zdenek.nemec@artin.cz>
2020-10-26 17:24:05 +0100

    #6: Continuous movement

--- Spaceship/Assets/Scripts/SpaceshipMovement.cs
+++ Spaceship/Assets/Scripts/SpaceshipMovement.cs
@@ -8,18 +8,18 @@ public class SpaceshipMovement : MonoBehaviour {
     }
 
     void Update() {
-        if (Input.GetKeyDown(KeyCode.W)) {
+        if (Input.GetKey(KeyCode.W)) {
             Debug.Log("SpaceshipMovement.Update(): W pressed");
-            transform.position = transform.position + new Vector3(0, .1f, 0);
-        } else if (Input.GetKeyDown(KeyCode.A)) {
+            transform.position = transform.position + new Vector3(0, .02f, 0);
+        } else if (Input.GetKey(KeyCode.A)) {
             Debug.Log("SpaceshipMovement.Update(): A pressed");
-            transform.position = transform.position + new Vector3(-.1f, 0, 0);
-        } else if (Input.GetKeyDown(KeyCode.S)) {
+            transform.position = transform.position + new Vector3(-.02f, 0, 0);
+        } else if (Input.GetKey(KeyCode.S)) {
             Debug.Log("SpaceshipMovement.Update(): S pressed");
-            transform.position = transform.position + new Vector3(0, -.1f, 0);
-        } else if (Input.GetKeyDown(KeyCode.D)) {
+            transform.position = transform.position + new Vector3(0, -.02f, 0);
+        } else if (Input.GetKey(KeyCode.D)) {
             Debug.Log("SpaceshipMovement.Update(): D pressed");
-            transform.position = transform.position + new Vector3(.1f, 0, 0);
+            transform.position = transform.position + new Vector3(.02f, 0, 0);
         }
     }
 }
