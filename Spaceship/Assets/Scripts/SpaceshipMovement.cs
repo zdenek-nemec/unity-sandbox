83802e07164b84573f081624e30407ed74f717ca
zdenek-nemec <zdenek.nemec@artin.cz>
2020-10-26 17:21:07 +0100

    #6: WASD step movement

---
+++ Spaceship/Assets/Scripts/SpaceshipMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceshipMovement : MonoBehaviour {
+    void Start() {
+        Debug.Log("SpaceshipMovement.Start()");
+    }
+
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.W)) {
+            Debug.Log("SpaceshipMovement.Update(): W pressed");
+            transform.position = transform.position + new Vector3(0, .1f, 0);
+        } else if (Input.GetKeyDown(KeyCode.A)) {
+            Debug.Log("SpaceshipMovement.Update(): A pressed");
+            transform.position = transform.position + new Vector3(-.1f, 0, 0);
+        } else if (Input.GetKeyDown(KeyCode.S)) {
+            Debug.Log("SpaceshipMovement.Update(): S pressed");
+            transform.position = transform.position + new Vector3(0, -.1f, 0);
+        } else if (Input.GetKeyDown(KeyCode.D)) {
+            Debug.Log("SpaceshipMovement.Update(): D pressed");
+            transform.position = transform.position + new Vector3(.1f, 0, 0);
+        }
+    }
+}
