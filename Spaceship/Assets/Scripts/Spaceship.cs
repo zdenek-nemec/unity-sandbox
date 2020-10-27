1d2293521e04c94b0f245d02c2e40a328573dfa2
zdenek-nemec <zdenek.nemec@artin.cz>
2020-10-27 07:01:29 +0100

    #6: Firing

--- Spaceship/Assets/Scripts/Spaceship.cs
+++ Spaceship/Assets/Scripts/Spaceship.cs
@@ -2,16 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpaceshipMovement : MonoBehaviour {
-    void Start() {
-        Debug.Log("SpaceshipMovement.Start()");
-        // TODO: Move spaceship to mid-bottom
-    }
+public class Spaceship : MonoBehaviour {
+    [SerializeField] private Weapon[] weapons;
+    [SerializeField] private float projectileSpeed = 1;
 
-    void Update() {
-        // Debug.Log("SpaceshipMovement.Update()");
-
-        // Movement
+    private void Update() {
         if (Input.GetKey(KeyCode.W)) {
             transform.position = transform.position + new Vector3(0, .02f, 0);
         }
@@ -25,9 +20,14 @@ public class SpaceshipMovement : MonoBehaviour {
             transform.position = transform.position + new Vector3(.02f, 0, 0);
         }
 
-        // Fire
         if (Input.GetKeyDown(KeyCode.Space)) {
-            Debug.Log("SpaceshipMovement.Update(): Space pressed");
+            Fire();
+        }
+    }
+
+    private void Fire() {
+        foreach (var weapon in weapons) {
+           weapon.Fire(projectileSpeed);
         }
     }
 }
---
+++ Spaceship/Assets/Scripts/Weapon.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour {
+    [SerializeField] private Projectile projectilePrefab;
+    [SerializeField] private Transform transformMuzzle;
+
+    public void Fire(float velocity) {
+        var projectile = Instantiate(projectilePrefab);
+        projectile.transform.position = transformMuzzle.position;
+        projectile.Launch(transformMuzzle.up * velocity);
+    }
+}
