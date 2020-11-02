838bf97f6018be0c8a5a947215b0261c4b2cd759
zdenek-nemec <zdenek.nemec@artin.cz>
2020-11-02 21:04:53 +0100

    #6: Preparations for velocity movement

--- Spaceship/Assets/Scripts/Spaceship.cs
+++ Spaceship/Assets/Scripts/Spaceship.cs
@@ -2,23 +2,35 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class Spaceship : MonoBehaviour {
     [SerializeField] private Weapon[] weapons;
     [SerializeField] private float projectileSpeed = 1;
+    private Vector3 zeroVelocity;
 
     private void Update() {
+        Rigidbody rigidbody = GetComponent<Rigidbody>();
+
         if (Input.GetKey(KeyCode.W)) {
             transform.position = transform.position + new Vector3(0, .02f, 0);
         }
         if (Input.GetKey(KeyCode.A)) {
-            transform.position = transform.position + new Vector3(-.1f, 0, 0);
+            transform.position = transform.position + new Vector3(-.02f, 0, 0);
         }
         if (Input.GetKey(KeyCode.S)) {
             transform.position = transform.position + new Vector3(0, -.02f, 0);
         }
         if (Input.GetKey(KeyCode.D)) {
-            transform.position = transform.position + new Vector3(.1f, 0, 0);
+            transform.position = transform.position + new Vector3(.02f, 0, 0);
         }
+
+        // if (Input.GetKeyDown(KeyCode.D)) {
+        //     zeroVelocity = rigidbody.velocity;
+        //     Vector3 velocity = transform.right * 1;
+        //     rigidbody.velocity = velocity;
+        // } else if (Input.GetKeyUp(KeyCode.D)) {
+        //     rigidbody.velocity = zeroVelocity;
+        // }
 
         if (Input.GetKeyDown(KeyCode.Space)) {
             Fire();
