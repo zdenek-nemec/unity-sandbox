b0a8cbab35b87664b251fd8f7ca3b26be7f02b60
zdenek-nemec <zdenek.nemec@artin.cz>
2020-12-07 20:22:56 +0100

    #9: Basic movement and other tanks

---
+++ Tanks/Assets/Scripts/Tank.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tank : MonoBehaviour {
+    void Update() {
+        Rigidbody rigidbody = GetComponent<Rigidbody>();
+
+        if (Input.GetKey(KeyCode.W)) {
+            rigidbody.velocity = transform.right * -3f;
+        }
+        if (Input.GetKey(KeyCode.S)) {
+            rigidbody.velocity = transform.right * 3f;
+        }
+
+        if (Input.GetKey(KeyCode.A)) {
+            transform.Rotate(0, -1f, 0);
+        }
+        if (Input.GetKey(KeyCode.D)) {
+            transform.Rotate(0, 1f, 0);
+        }
+    }
+}
