f69de332da21075c7333e6a02dee5609a404d937
zdenek-nemec <zdenek.nemec@artin.cz>
2020-12-14 21:06:58 +0100

    #9: Projectile

---
+++ Tanks/Assets/Scripts/Projectile.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour {
+    void Start() {
+    }
+
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.X)) {
+            transform.position = new Vector3(0, .25f, -2f);
+            transform.rotation = new Quaternion(0, 0, 0, 0);
+            Rigidbody rigidbody = GetComponent<Rigidbody>();
+            rigidbody.velocity = transform.forward * 10f;
+        }
+    }
+}
