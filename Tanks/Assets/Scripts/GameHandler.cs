b6be1229714c4ec8a77521fcbf543189d37955d2
zdenek-nemec <zdenek.nemec@artin.cz>
2020-12-15 23:32:58 +0100

    #9: Second player, refactoring

--- Tanks/Assets/Scripts/GameHandler.cs
+++ Tanks/Assets/Scripts/GameHandler.cs
@@ -4,20 +4,30 @@ using UnityEngine;
 
 public class GameHandler : MonoBehaviour {
     [SerializeField] private Tank tankPrefab;
+    [SerializeField] private Material player1Material;
+    [SerializeField] private Material player2Material;
 
     private void Start() {
         Debug.Log("GameHandler.Start");
 
         // Create tanks and randomly assign the player
-        int playerTankIndex = Random.Range(0, 8);
+        int player1Index = Random.Range(0, 9);
+        int player2Index = Random.Range(0, 9);
+        while (player2Index == player1Index) {
+            player2Index = Random.Range(0, 9);
+        }
+
         int index = 0;
         for (int x = -2; x <= 2; x += 2) {
             for (int z = -2; z <= 2; z += 2) {
                 var tank = Instantiate(tankPrefab);
                 tank.transform.position = new Vector3(x, 1f, z);
-                if (index++ == playerTankIndex) {
-                    tank.AssumeControl();
+                if (index == player1Index) {
+                    tank.AssumeControl("WASD", player1Material);
+                } else if (index == player2Index) {
+                    tank.AssumeControl("Arrows", player2Material);
                 }
+                index++;
             }
         }
     }
--- Tanks/Assets/Scripts/Tank.cs
+++ Tanks/Assets/Scripts/Tank.cs
@@ -3,53 +3,79 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Tank : MonoBehaviour {
-    [SerializeField] private Material playerMaterial;
     [SerializeField] private Projectile projectilePrefab;
-    private bool controls = false;
+    private Material material = null;
+    private bool controlsWasd = false;
+    private bool controlsArrows = false;
     private int speedMultiplier = 1;
 
-    public void AssumeControl() {
-        controls = true;
+    public void AssumeControl(string controls, Material material) {
+        if (controls == "WASD") {
+            controlsWasd = true;
+        } else if (controls == "Arrows") {
+            controlsArrows = true;
+        }
+
+        this.material = material;
+
         foreach (Transform child in transform) {
             if (!child.name.Equals("Muzzle")) {
-                child.GetComponent<MeshRenderer>().material = playerMaterial;
+                child.GetComponent<MeshRenderer>().material = material;
             }
         }
     }
 
-    public void ReleaseControl() {
-        controls = false;
-    }
-
     private void Update() {
-        if (!controls) {
-            return;
-        }
-
         Rigidbody rigidbody = GetComponent<Rigidbody>();
 
-        if (Input.GetKey(KeyCode.W)) {
-            rigidbody.velocity = transform.forward * 3f * speedMultiplier;
-        }
-        if (Input.GetKey(KeyCode.S)) {
-            rigidbody.velocity = transform.forward * -3f;
-        }
+        if (controlsWasd) {
+            if (Input.GetKey(KeyCode.W)) {
+                rigidbody.velocity = transform.forward * 3f * speedMultiplier;
+            }
+            if (Input.GetKey(KeyCode.S)) {
+                rigidbody.velocity = transform.forward * -3f;
+            }
 
-        if (Input.GetKey(KeyCode.A)) {
-            transform.Rotate(0, -1f, 0);
-        }
-        if (Input.GetKey(KeyCode.D)) {
-            transform.Rotate(0, 1f, 0);
-        }
+            if (Input.GetKey(KeyCode.A)) {
+                transform.Rotate(0, -1f, 0);
+            }
+            if (Input.GetKey(KeyCode.D)) {
+                transform.Rotate(0, 1f, 0);
+            }
 
-        if (Input.GetKeyDown(KeyCode.LeftShift)) {
-            speedMultiplier = 2;
-        } else if (Input.GetKeyUp(KeyCode.LeftShift)) {
-            speedMultiplier = 1;
-        }
+            if (Input.GetKeyDown(KeyCode.LeftShift)) {
+                speedMultiplier = 2;
+            } else if (Input.GetKeyUp(KeyCode.LeftShift)) {
+                speedMultiplier = 1;
+            }
 
-        if (Input.GetKeyDown(KeyCode.Space)) {
-            Fire();
+            if (Input.GetKeyDown(KeyCode.Space)) {
+                Fire();
+            }
+        } else if (controlsArrows) {
+            if (Input.GetKey(KeyCode.UpArrow)) {
+                rigidbody.velocity = transform.forward * 3f * speedMultiplier;
+            }
+            if (Input.GetKey(KeyCode.DownArrow)) {
+                rigidbody.velocity = transform.forward * -3f;
+            }
+
+            if (Input.GetKey(KeyCode.LeftArrow)) {
+                transform.Rotate(0, -1f, 0);
+            }
+            if (Input.GetKey(KeyCode.RightArrow)) {
+                transform.Rotate(0, 1f, 0);
+            }
+
+            if (Input.GetKeyDown(KeyCode.RightShift)) {
+                speedMultiplier = 2;
+            } else if (Input.GetKeyUp(KeyCode.RightShift)) {
+                speedMultiplier = 1;
+            }
+
+            if (Input.GetKeyDown(KeyCode.RightControl)) {
+                Fire();
+            }
         }
     }
 
@@ -57,7 +83,7 @@ public class Tank : MonoBehaviour {
         var projectile = Instantiate(projectilePrefab);
         foreach (Transform child in transform) {
             if (child.name.Equals("Muzzle")) {
-                projectile.Launch(child, playerMaterial);
+                projectile.Launch(child, this.material);
                 break;
             }
         }
